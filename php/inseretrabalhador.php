363f06d16da1a0b1535f8cd026c5b33b6533a3c9
Evairlf <evairleitefeldmann@gmail.com>
2021-04-10 00:49:14 -0300

    adcionei acesso e inserção ao banco de dados

--- php/inseretrabalhador.php
+++ php/inseretrabalhador.php
@@ -11,12 +11,24 @@ if(isset($_POST['dataEntrada']) || isset($_POST['horaEntrada']) || isset($_POST[
     $horaEntrada = $_POST['horaEntrada'];
     $horaSaida = $_POST['horaSaida'];
     $Justificativa = $_POST['Justificativa'];
-    echo 'Hora de Saida: '.$horaSaida.'<br>';
+   /* echo 'Hora de Saida: '.$horaSaida.'<br>';
     echo 'Hora Entrada: '.$horaEntrada.'<br>';
     echo 'Data: '.$dataEntrada.'<br>';
-    echo 'Justificativa: '.$Justificativa.'<br>';
+    echo 'Justificativa: '.$Justificativa.'<br>';*/
 }
 
 
-$sql = 'INSERT INTO horaTrabahador (usuario,senha) VALUES (?,?)';
+$sql = 'INSERT INTO horaTrabalhador (dataAtual,horaEntrada,horaSaida,Justificativa) VALUES (?,?,?,?)';
+
+$stms = $conn->prepare($sql);
+$stms->bindParam(1,$dataEntrada);
+$stms->bindParam(2,$horaEntrada);
+$stms->bindParam(3,$horaSaida);
+$stms->bindParam(4,$Justificativa);
+
+if($stms->execute()){
+    echo 'Dados inseridos com sucesso!';
+}else{
+    echo 'Deu Alguma Merda';
+}
 
